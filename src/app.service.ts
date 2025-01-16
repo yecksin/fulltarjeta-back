b1fcf619d90bfdb066ce0a28b485d8678b8ced86
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-01-15 22:44:45 -0500

    :sparkles: feat: supabase added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient } from '@supabase/supabase-js';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private readonly configService: ConfigService) {}
+  supabase = createClient(
+    this.configService.get('SUPABASE_URL'),
+    this.configService.get('SUPABASE_KEY'),
+  );
+  async getHello(): Promise<string> {
+    const res = await this.supabase.from('subdomains').select('*');
+    console.log(res);
     return 'Hello World! from railway';
   }
 }
