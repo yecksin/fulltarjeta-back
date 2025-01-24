a3c65ad79ac5716425883bdbcdb1e05815c4672e
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-01-24 12:07:02 -0500

    :sparkles: feat: worker crud added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MercadopagoModule } from './mercadopago/mercadopago.module';
+import { WorkerModule } from './worker/worker.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { MercadopagoModule } from './mercadopago/mercadopago.module';
       isGlobal: true,
     }),
     MercadopagoModule,
+    WorkerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
